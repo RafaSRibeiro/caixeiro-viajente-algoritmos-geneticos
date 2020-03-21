7ac7a211cc9af7ef7d369e4465ff8345992aa4a5
Rafael Ribeiro <rafael.s.ribeiro@sc.senai.br>
2020-03-21 14:55:47 -0300

    Criação de tela e model

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,4 +1,38 @@
 package app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import model.Cidade;
+
+import java.util.ArrayList;
+
 public class Controller {
+
+    ArrayList<Cidade> cidades = new ArrayList<Cidade>();
+
+    @FXML
+    TextField nomeCidade;
+
+    @FXML
+    TextField cidadeInicial;
+
+    @FXML
+    TextField cidadeFinal;
+
+    @FXML
+    TextField distancia;
+
+    @FXML
+    TextArea console;
+
+    @FXML
+    public void newCidade() {
+
+    }
+
+    @FXML
+    public void newDistancia() {
+
+    }
 }
---
+++ src/model/Cidade.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Cidade {
+
+    private String nome;
+
+    public Cidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
